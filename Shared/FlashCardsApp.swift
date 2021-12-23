3b4cdd78a772bc0c507380a8fc778c83aeb97c47
dominikmolitor <dominik-jakob.molitor@protonmail.com>
2021-12-23 01:12:26 +0100

    Initial Commit

---
+++ Shared/FlashCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashCardsApp.swift
+//  Shared
+//
+//  Created by Dominik Molitor on 23.12.21.
+//
+
+import SwiftUI
+
+@main
+struct FlashCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
